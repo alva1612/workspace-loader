9ee5274cf512fb37affaef9cec3d06eb71c438c0
Alvaro <alvaro.guillenc1612@gmail.com>
2023-10-11 16:08:14 -0500

    added graph ql course workspace

--- script.sh
+++ script.sh
@@ -2,8 +2,10 @@
 PWD="/home/alvaro/Documents/Proyectos/bash/workspace-loader"
 DELFOSTI="/home/alvaro/Documents/Delfosti"
 PERSONAL="/home/alvaro/Documents/Proyectos"
+CURSOS="/home/alvaro/Documents/Estudios/Cursos"
 export DELFOSTI
 export PERSONAL
+export CURSOS
 
 SCRIPT_NOT_FOUND="Workspace script not found"
 
@@ -22,6 +24,11 @@ if [ -d  "$PWD/workspaces" ]; then
     
     "magire") if [ -f "$PWD/workspaces/magire.sh" ]; then
                 sh "$PWD/workspaces/magire.sh"
+            else
+                echo $SCRIPT_NOT_FOUND
+            fi;;
+    "c-graphql") if [ -f "$PWD/workspaces/curso-graphql.sh" ]; then
+                sh "$PWD/workspaces/curso-graphql.sh"
             else
                 echo $SCRIPT_NOT_FOUND
             fi;;
--- workspaces/magire.sh
+++ workspaces/magire.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
-cd $PERSONAL/magire/serverless
+cd $PERSONAL/magire/user-manager
 codium .
